1aab974c5b51af4119c041e454a1db5cc8c19440
mazbrili <mazbrili@yahoo.com>
2026-05-04 16:59:18 +0700

    Update genericstrip.sh

--- builder-utils/genericstrip.sh
+++ builder-utils/genericstrip.sh
@@ -1,94 +1,70 @@
 #!/bin/bash
 
+# Fungsi bantu untuk membersihkan path di dalam direktori program Gobo
 GenericStrip() {
-	rm usr/share/pixmaps/*.xpm
-	rm var/log/removed_packages
-	rm var/log/removed_scripts
-	rm var/log/removed_uninstall_scripts
+	# GoboLinux menggunakan 'Shared' sebagai ganti 'usr/share'
+	rm -f Shared/pixmaps/*.xpm
+	
+	# Hapus metadata dan log (GoboLinux menyimpan settings di System/Settings)
+	rm -rf etc/bash_completion*
+	rm -rf etc/logrotate.d
 
-	rm -fr etc/bash_completion*
-	rm -fr etc/logrotate.d
-	rm -fr usr/doc
-	rm -fr usr/include
-	rm -fr usr/info
-	rm -fr usr/lib*/cmake
-	rm -fr usr/lib*/gtk*/include
-	rm -fr usr/lib*/pkgconfig
-	rm -fr usr/lib*/python2*
-	rm -fr usr/lib*/systemd
-	rm -fr usr/lib/python*/site-packages/*-info
-	rm -fr usr/libexec/installed-tests
-	rm -fr usr/man
-	rm -fr usr/share/*/translations
-	rm -fr usr/share/aclocal
-	rm -fr usr/share/bash-completion
-	rm -fr usr/share/cmake
-	rm -fr usr/share/devhelp
-	rm -fr usr/share/doc
-	rm -fr usr/share/fish
-	rm -fr usr/share/gdb
-	rm -fr usr/share/gir-1.0
-	rm -fr usr/share/gtk-doc
-	rm -fr usr/share/help
-	rm -fr usr/share/icons/HighContrast
-	rm -fr usr/share/icons/hicolor/64x64
-	rm -fr usr/share/icons/hicolor/72x72
-	rm -fr usr/share/icons/hicolor/96x96
-	rm -fr usr/share/icons/hicolor/192x192
-	rm -fr usr/share/info
-	rm -fr usr/share/installed-tests
-	rm -fr usr/share/locale
-	rm -fr usr/share/man
-	rm -fr usr/share/metainfo/
-	rm -fr usr/share/pkgconfig
-	rm -fr usr/share/sounds
-	rm -fr usr/share/themes/HighContrast
-	rm -fr usr/share/vala
-	rm -fr usr/share/xdg-terminals
-	rm -fr usr/share/zsh
-	rm -fr usr/src
-	rm -fr var/lib/pkgtools/douninst.sh/
-	rm -fr var/lib/pkgtools/setup
-	rm -fr var/log/pkgtools
-	rm -fr var/log/setup
-	rm -fr var/man
-
+	# Hapus header, dokumentasi, dan info (setara usr/doc, usr/include, dll)
+	rm -rf doc
+	rm -rf include
+	rm -rf info
+	rm -rf Shared/doc
+	rm -rf Shared/info
+	rm -rf Shared/man
+	rm -rf Shared/help
+	rm -rf Shared/locale
+	
+	# Hapus file development di dalam library
+	# lib di Gobo berada langsung di root program atau di bawah /lib
+	rm -rf lib/cmake
+	rm -rf lib/pkgconfig
+	rm -rf lib/python*/site-packages/*-info
+	
+	# Hapus file integrasi sistem yang tidak perlu untuk versi lite
+	rm -rf Shared/bash-completion
+	rm -rf Shared/cmake
+	rm -rf Shared/gir-1.0
+	rm -rf Shared/vala
+	
+	# Bersihkan file source dan build artifacts secara rekursif
 	find . -name '*.a' -delete
 	find . -name '*.c' -delete
 	find . -name '*.cpp' -delete
-	find . -name '*.cmake' -delete
-	find . -name '*.deps' -delete
-	find . -name '*.gir' -delete
 	find . -name '*.h' -delete
 	find . -name '*.hpp' -delete
 	find . -name '*.la' -delete
-	find . -name '*.m4' -delete
-	find . -name '*.make' -delete
-	find . -name '*.mk' -delete
-	find . -name '*.o' -delete
 	find . -name '*.pc' -delete
-	find . -name '*.prl' -delete
-	find . -name '*.vapi' -delete
 	find . -name 'AUTHORS*' -delete
 	find . -name 'COPYING*' -delete
 	find . -name 'LICENSE*' -delete
 	find . -name 'README*' -delete
 
-	find usr/ -type d -empty -delete
+	# Hapus direktori kosong (khas Gobo: folder Executable, Shared, dll jika kosong)
+	find . -type d -empty -delete
 
-	find usr/share/mime/ -mindepth 1 -maxdepth 1 -not -name packages -exec rm -rf '{}' \; 2>/dev/null
-
-	find . | xargs file | egrep -e "executable|shared object" | grep ELF | cut -f 1 -d : | xargs strip --strip-debug --strip-unneeded -R .comment* -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
+	# Stripping ELF (Library dan Executable)
+	# Menggunakan standar strip untuk membuang simbol debug dan note
+	find . -type f | xargs file | grep ELF | cut -f 1 -d : | xargs strip --strip-debug --strip-unneeded -R .comment* -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.GNU-stack 2> /dev/null
 } > /dev/null 2>&1
 
 AggressiveStrip() {
+	# Deteksi fitur strip-section-headers (untuk memperkecil ukuran secara ekstrem)
 	[[ $(strip --help | grep "strip-section-headers") ]] && stripSectionHeaders="--strip-section-headers"
-	find . | xargs file | egrep -e "executable" | grep ELF | cut -f 1 -d : | xargs strip --strip-all ${stripSectionHeaders} -R .comment* -R .eh_frame* -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
+	
+	# Fokus pada Executable di Gobo (biasanya di folder bin/ atau sbin/)
+	find . -type f | xargs file | grep "executable" | grep ELF | cut -f 1 -d : | xargs strip --strip-all ${stripSectionHeaders} -R .comment* -R .eh_frame* -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.GNU-stack 2> /dev/null
 } > /dev/null 2>&1
 
 AggressiveStripAll() {
 	[[ $(strip --help | grep "strip-section-headers") ]] && stripSectionHeaders="--strip-section-headers"
-	find . | xargs file | egrep -e "executable|shared object" | grep ELF | cut -f 1 -d : | xargs strip --strip-all ${stripSectionHeaders} -R .comment* -R .eh_frame* -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
+	
+	# Sikat habis semua ELF (Executable & Shared Object/Library)
+	find . -type f | xargs file | grep ELF | cut -f 1 -d : | xargs strip --strip-all ${stripSectionHeaders} -R .comment* -R .eh_frame* -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.GNU-stack 2> /dev/null
 } > /dev/null 2>&1
 
 if [ "$1" ]; then
